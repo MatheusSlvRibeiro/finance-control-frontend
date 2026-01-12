6588de93f6ab53779636b23c943545a4182cf1e3
Matheus da Silva <silva.contadev@gmail.com>
2026-01-12 16:56:54 -0300

    :iphone: feat(stats-card): make stats card responsive for tablets and desktops

--- app/(private)/dashboard/_components/statsCard/statsCard.tsx
+++ app/(private)/dashboard/_components/statsCard/statsCard.tsx
@@ -1,15 +1,30 @@
-import { Landmark, Minus, Plus } from "lucide-react";
-import IMask from "imask";
+"use client";
+
+import { ArrowRightLeft, Landmark, Minus, Plus } from "lucide-react";
 import styles from "./statsCard.module.scss";
 import { formatCurrency } from "../../../../../utils/formatCurrency";
+import { useMediaQuery } from "react-responsive";
 
 export function StatsCard() {
-	type StatsLabel = "Contas" | "Receitas" | "Despesas";
+	type StatsLabel = "Contas" | "Receitas" | "Despesas" | "Transferências";
 
-	const iconsClassMap: Record<StatsLabel, string> = {
-		Contas: styles.icon_accounts,
-		Receitas: styles.icon_incomes,
-		Despesas: styles.icon_expenses,
+	const classMap: Record<StatsLabel, { card: string; icon: string }> = {
+		Contas: {
+			card: styles.content_account,
+			icon: styles.icon_accounts,
+		},
+		Receitas: {
+			card: styles.content_incomes,
+			icon: styles.icon_incomes,
+		},
+		Despesas: {
+			card: styles.content_expenses,
+			icon: styles.icon_expenses,
+		},
+		Transferências: {
+			card: styles.content_transfers,
+			icon: styles.icon_transfers,
+		},
 	};
 
 	const stats: { label: StatsLabel; amount: number; icon: JSX.Element }[] = [
@@ -28,24 +43,32 @@ export function StatsCard() {
 			amount: 3750,
 			icon: <Minus />,
 		},
+		{
+			label: "Transferências",
+			amount: 0,
+			icon: <ArrowRightLeft />,
+		},
 	];
+
+	const isTablet = useMediaQuery({ minWidth: 576 });
 
 	return (
 		<section className={styles.stats_card__container}>
-
-			<p className={styles.stats_card__title}>Visão Geral</p>
+			{isTablet ? (
+				""
+			) : (
+				<p className={styles.stats_card__title}>Visão Geral</p>
+			)}
 
 			{stats.map((item) => (
-				<div key={item.label} className={styles.stats_card__content}>
+				<div key={item.label} className={classMap[item.label].card}>
+					<div className={classMap[item.label].icon}>{item.icon}</div>
 					<div className={styles.stats_card__type}>
-						<div className={iconsClassMap[item.label]}>
-							{item.icon}
-						</div>
 						<p className={styles.stats_card_label}>{item.label}</p>
+						<p className={styles.stats_card_amount}>
+							{formatCurrency(item.amount)}
+						</p>
 					</div>
-					<p className={styles.stats_card_amount}>
-						{formatCurrency(item.amount)}
-					</p>
 				</div>
 			))}
 		</section>
