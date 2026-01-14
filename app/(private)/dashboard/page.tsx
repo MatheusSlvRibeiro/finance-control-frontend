a5c828773000da284f4d8838481e71dbb04db514
Matheus da Silva <silva.contadev@gmail.com>
2026-01-14 16:10:30 -0300

    :sparkles: feat(latest-expenses): add LatestExpenses table component
    - Add new LatestExpenses component, responsible for rendering mobile and desktop versions based on breakpoints
    - Add new subcomponents: LatestExpensesDesktop and LatestExpensesMobile, each with its own SCSS file

--- app/(private)/dashboard/page.tsx
+++ app/(private)/dashboard/page.tsx
@@ -5,9 +5,9 @@ import styles from "./page.module.scss";
 import { StatsCard } from "./_components/statsCard/statsCard";
 import { ExpenseChart } from "./_components/ExpenseChart/ExpenseChart";
 import { CategoryChart } from "./_components/CategoryChart/CategoryChart";
+import { LatestExpenses } from "./_components/LatestExpenses/LatestExpenses";
 
 export default function Dashboard() {
-
 	return (
 		<div className={styles.dashboard__container}>
 			<PageHeader
@@ -18,19 +18,14 @@ export default function Dashboard() {
 			<StatsCard />
 
 			<section className={styles.dashboard_charts__container}>
-				<div className={styles.dashboard__lineChart}>
-					<h3 className={styles.lineChart_title}>Evolução Financeira</h3>
+				<ExpenseChart />
 
-					<ExpenseChart />
-				</div>
-
-				<div className={styles.dashboard__pieChart}>
-					<h3 className={styles.pieChart_title}>Despesas por Categoria</h3>
-					<CategoryChart />
-				</div>
+				<CategoryChart />
 			</section>
 
-			<section></section>
+			<section>
+				<LatestExpenses />
+			</section>
 		</div>
 	);
 }
