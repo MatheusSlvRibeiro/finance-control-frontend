46827dadec7632ade54451ccd32322530e61466c
Matheus da Silva <silva.contadev@gmail.com>
2026-01-16 21:08:33 -0300

    :sparkles: feat(alias): add @data path alias

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
 			"@components": path.resolve(__dirname, "src/components"),
+			"@data": path.resolve(__dirname, "src/data"),
 			"@hooks": path.resolve(__dirname, "src/hooks"),
 			"@pages": path.resolve(__dirname, "src/pages"),
 			"@styles": path.resolve(__dirname, "src/styles"),
